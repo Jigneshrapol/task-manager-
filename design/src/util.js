6a01625f332ff181e509a4f7ce15b98c094f9ee8
Jignesh Rapol <138034895+Jigneshrapol@users.noreply.github.com>
2024-07-15 20:03:42 +0530

    Update util.js

--- design/src/util.js
+++ design/src/util.js
@@ -1,6 +1,6 @@
 import { toast } from "react-hot-toast";
 
-export const BASE_URL="http://localhost:8081";
+export const BASE_URL="https://task-manager-api-liart.vercel.app";
 export const notifysuccess = (message,type)=>{
     toast.success(message,{duration:3000,position:'top-right'});
 }
